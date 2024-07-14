ba4c8a41d60991392985f4fdac8f1702dc4e54d3
Solaiyappan S <solaiyappatce@gmail.com>
2024-07-14 22:57:30 +0530

    (feat) : Initial Game setup

---
+++ main.js
@@ -0,0 +1,61 @@
+var isXTurn = true;
+var co_ordinates = [
+  [-1, -1],
+  [-1, -1],
+  [-1, -1],
+  [-1, -1],
+  [-1, -1],
+  [-1, -1],
+];
+var isAvailable = [
+  [true, true, true],
+  [true, true, true],
+  [true, true, true],
+];
+
+const pickCell = (x, y) => {
+  if (isAvailable[x][y]) {
+    if (co_ordinates[0][0] != -1 && co_ordinates[0][1] != -1) {
+      $(`#gameCell_${co_ordinates[0][0]}_${co_ordinates[0][1]}`)
+        .css("color", "#0000")
+        .html("&nbsp;");
+      isAvailable[co_ordinates[0][0]][co_ordinates[0][1]] = true;
+    }
+    for (var i = 0; i < 5; i++) {
+      co_ordinates[i] = co_ordinates[i + 1];
+    }
+    co_ordinates[5] = [x, y];
+    isAvailable[x][y] = false;
+    if (co_ordinates[0][0] != -1 && co_ordinates[0][1] != -1) {
+      $(`#gameCell_${co_ordinates[0][0]}_${co_ordinates[0][1]}`).css(
+        "color",
+        isXTurn ? "#ff04" : "#0ff4"
+      );
+      isAvailable[co_ordinates[0][0]][co_ordinates[0][1]] = true;
+    }
+    $(`#gameCell_${x}_${y}`)
+      .css("color", isXTurn ? "#0ff" : "#ff0")
+      .html(isXTurn ? "X" : "O");
+    isXTurn = !isXTurn;
+  }
+};
+
+const refreshBoard = () => {
+  for (var i = 0; i < 3; i++)
+    for (var j = 0; j < 3; j++)
+      $(`#gameCell_${i}_${j}`).css("color", "#0000").html("&nbsp;");
+  isXTurn = true;
+  co_ordinates = [
+    [-1, -1],
+    [-1, -1],
+    [-1, -1],
+    [-1, -1],
+    [-1, -1],
+    [-1, -1],
+  ];
+  isAvailable = [
+    [true, true, true],
+    [true, true, true],
+    [true, true, true],
+  ];
+};

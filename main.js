e4944351dcada509e13d5a1f3af95eec4c45a95e
Solaiyappan S <solaiyappatce@gmail.com>
2024-08-08 23:04:44 +0530

    latest logic for refresh button

--- main.js
+++ main.js
@@ -13,6 +13,11 @@ var isAvailable = [
   [true, true, true],
 ];
 var canRefresh = false;
+var gameCells = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
 var gameWin = false;
 var gameWinCells = [
   [-1, -1],
@@ -20,6 +25,7 @@ var gameWinCells = [
   [-1, -1],
 ];
 var winColor = "#0000";
+var blocked_cell = [-1, -1];
 
 $(document).ready(function () {
   for (var i = 0; i < 3; i++)
@@ -30,7 +36,11 @@ $(document).ready(function () {
 });
 
 const pickCell = (x, y) => {
-  if (isAvailable[x][y] && !gameWin) {
+  if (
+    isAvailable[x][y] &&
+    !(blocked_cell[0] === x && blocked_cell[1] === y) &&
+    !gameWin
+  ) {
     if (co_ordinates[0][0] != -1 && co_ordinates[0][1] != -1) {
       $(`#gameCell_${co_ordinates[0][0]}_${co_ordinates[0][1]}`)
         .css("color", "#0000")
@@ -49,6 +59,8 @@ const pickCell = (x, y) => {
         isXTurn ? "#ff04" : "#0ff4"
       );
       isAvailable[co_ordinates[0][0]][co_ordinates[0][1]] = true;
+      blocked_cell[0] = co_ordinates[0][0];
+      blocked_cell[1] = co_ordinates[0][1];
     }
     $(`#gameCell_${x}_${y}`)
       .css("color", isXTurn ? "#0ff" : "#ff0")
@@ -57,6 +69,21 @@ const pickCell = (x, y) => {
   }
   checkGameWin();
   $("#refreshBtn").css("opacity", canRefresh ? "1" : "0.3");
+};
+
+const isWin = (c1, c2, c3) =>
+  gameCells[c1[0]][c1[1]] === gameCells[c2[0]][c2[1]] &&
+  gameCells[c2[0]][c2[1]] === gameCells[c3[0]][c3[1]] &&
+  gameCells[c1[0]][c1[1]] !== "&nbsp;";
+
+const setWin = (c1, c2, c3) => {
+  gameWin = true;
+  gameWinCells = [
+    [c1[0], c1[1]],
+    [c2[0], c2[1]],
+    [c3[0], c3[1]],
+  ];
+  winColor = gameCells[c1[0]][c1[1]] === "X" ? "#0ff" : "#ff0";
 };
 
 const refreshBoard = () => {
@@ -83,116 +110,26 @@ const refreshBoard = () => {
     canRefresh = false;
     gameWin = false;
   }
-  $("#refreshBtn").css("opacity", canRefresh ? "1" : "0.3");
+  $("#refreshBtn").css({
+    opacity: canRefresh ? "1" : "0.3",
+    textShadow: "none",
+  });
 };
 
 const checkGameWin = () => {
-  var gameCells = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ];
   for (var i = 0; i < 3; i++)
     for (var j = 0; j < 3; j++)
       gameCells[i][j] = $(`#gameCell_${i}_${j}`).html();
 
-  if (
-    gameCells[0][0] === gameCells[0][1] &&
-    gameCells[0][1] === gameCells[0][2] &&
-    gameCells[0][0] !== "&nbsp;"
-  ) {
-    gameWin = true;
-    gameWinCells = [
-      [0, 0],
-      [0, 1],
-      [0, 2],
-    ];
-    winColor = gameCells[0][0] === "X" ? "#0ff" : "#ff0";
-  } else if (
-    gameCells[1][0] === gameCells[1][1] &&
-    gameCells[1][1] === gameCells[1][2] &&
-    gameCells[1][0] !== "&nbsp;"
-  ) {
-    gameWin = true;
-    gameWinCells = [
-      [1, 0],
-      [1, 1],
-      [1, 2],
-    ];
-    winColor = gameCells[1][0] === "X" ? "#0ff" : "#ff0";
-  } else if (
-    gameCells[2][0] === gameCells[2][1] &&
-    gameCells[2][1] === gameCells[2][2] &&
-    gameCells[2][0] !== "&nbsp;"
-  ) {
-    gameWin = true;
-    gameWinCells = [
-      [2, 0],
-      [2, 1],
-      [2, 2],
-    ];
-    winColor = gameCells[2][0] === "X" ? "#0ff" : "#ff0";
-  } else if (
-    gameCells[0][0] === gameCells[1][0] &&
-    gameCells[1][0] === gameCells[2][0] &&
-    gameCells[0][0] !== "&nbsp;"
-  ) {
-    gameWin = true;
-    gameWinCells = [
-      [0, 0],
-      [1, 0],
-      [2, 0],
-    ];
-    winColor = gameCells[0][0] === "X" ? "#0ff" : "#ff0";
-  } else if (
-    gameCells[0][1] === gameCells[1][1] &&
-    gameCells[1][1] === gameCells[2][1] &&
-    gameCells[0][1] !== "&nbsp;"
-  ) {
-    gameWin = true;
-    gameWinCells = [
-      [0, 1],
-      [1, 1],
-      [2, 1],
-    ];
-    winColor = gameCells[0][1] === "X" ? "#0ff" : "#ff0";
-  } else if (
-    gameCells[0][2] === gameCells[1][2] &&
-    gameCells[1][2] === gameCells[2][2] &&
-    gameCells[0][2] !== "&nbsp;"
-  ) {
-    gameWin = true;
-    gameWinCells = [
-      [0, 2],
-      [1, 2],
-      [2, 2],
-    ];
-    winColor = gameCells[0][2] === "X" ? "#0ff" : "#ff0";
-  } else if (
-    gameCells[0][0] === gameCells[1][1] &&
-    gameCells[1][1] === gameCells[2][2] &&
-    gameCells[0][0] !== "&nbsp;"
-  ) {
-    gameWin = true;
-    gameWinCells = [
-      [0, 0],
-      [1, 1],
-      [2, 2],
-    ];
-    winColor = gameCells[0][0] === "X" ? "#0ff" : "#ff0";
-  } else if (
-    gameCells[0][2] === gameCells[1][1] &&
-    gameCells[1][1] === gameCells[2][0] &&
-    gameCells[2][0] !== "&nbsp;"
-  ) {
-    gameWin = true;
-    gameWinCells = [
-      [0, 2],
-      [1, 1],
-      [2, 0],
-    ];
-    winColor = gameCells[0][2] === "X" ? "#0ff" : "#ff0";
-  } else {
+  if (isWin([0, 0], [0, 1], [0, 2])) setWin([0, 0], [0, 1], [0, 2]);
+  else if (isWin([1, 0], [1, 1], [1, 2])) setWin([1, 0], [1, 1], [1, 2]);
+  else if (isWin([2, 0], [2, 1], [2, 2])) setWin([2, 0], [2, 1], [2, 2]);
+  else if (isWin([0, 0], [1, 0], [2, 0])) setWin([0, 0], [1, 0], [2, 0]);
+  else if (isWin([0, 1], [1, 1], [2, 1])) setWin([0, 1], [1, 1], [2, 1]);
+  else if (isWin([0, 2], [1, 2], [2, 2])) setWin([0, 2], [1, 2], [2, 2]);
+  else if (isWin([0, 0], [1, 1], [2, 2])) setWin([0, 0], [1, 1], [2, 2]);
+  else if (isWin([0, 2], [1, 1], [2, 0])) setWin([0, 2], [1, 1], [2, 0]);
+  else {
     gameWin = false;
     gameWinCells = [
       [-1, -1],
@@ -204,9 +141,13 @@ const checkGameWin = () => {
   if (gameWin) {
     for (var i = 0; i < 3; i++) {
       $(`#gameCell_${gameWinCells[i][0]}_${gameWinCells[i][1]}`).css({
-        color: "white",
-        textShadow: `0 0 1.5vh ${winColor}`,
+        color: "#fffa",
+        textShadow: `0 0 5px ${winColor}, 0 0 10px ${winColor}, 0 0 20px ${winColor}, 0 0 40px #fff`,
       });
     }
+    $("#refreshBtn").css({
+      opacity: canRefresh ? "1" : "0.3",
+      textShadow: `0 0 5px ${winColor}, 0 0 10px ${winColor}, 0 0 20px ${winColor}, 0 0 40px #fff`,
+    });
   }
 };
